cdcff18d3a6cec869352d3d001b9c244657fd563
Broveku <gearhearjordan@gmail.com>
2021-07-05 13:51:48 -0700

    finished project and matched websites

--- js/index.js
+++ js/index.js
@@ -44,25 +44,62 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 
 
-const servicesLink = document.querySelector('.container a:nth-of-type(1)')
-servicesLink.textContent = 'Services'
+const servicesLink = document.querySelector('a:nth-of-type(1)')
+servicesLink.textContent = siteContent['nav']['nav-item-1']
 
-const productLink = document.querySelector('.container a:nth-of-type(2)')
-productLink.textContent = 'Product'
+const productLink = document.querySelector('a:nth-of-type(2)')
+productLink.textContent = siteContent['nav']['nav-item-2']
 
-const visionLink = document.querySelector('.container a:nth-of-type(3)')
-visionLink.textContent = 'Vision'
+const visionLink = document.querySelector('a:nth-of-type(3)')
+visionLink.textContent = siteContent['nav']['nav-item-3']
 
-const featureLink = document.querySelector('.container a:nth-of-type(4)')
-featureLink.textContent = 'Feature'
+const featureLink = document.querySelector('a:nth-of-type(4)')
+featureLink.textContent = siteContent['nav']['nav-item-4']
 
-const aboutLink = document.querySelector('.container a:nth-of-type(5)')
-aboutLink.textContent = 'About'
+const aboutLink = document.querySelector('a:nth-of-type(5)')
+aboutLink.textContent = siteContent['nav']['nav-item-5']
 
-const contactLink = document.querySelector('.container a:nth-of-type(6)')
-contactLink.textContent = 'Content'
+const contactLink = document.querySelector('a:nth-of-type(6)')
+contactLink.textContent = siteContent['nav']['nav-item-6']
 
 
 const h1Title = document.querySelector('.cta h1')
-
 h1Title.textContent = 'Dom Is Awesome'
+
+const newButton = document.querySelector('.cta-text button')
+newButton.textContent = 'Get Started'
+
+const topImg = document.querySelector('.cta img')
+topImg.src = 'img/header-img.png'
+
+const topContentH = document.querySelectorAll('.top-content h4')
+topContentH[0].textContent = siteContent['main-content"]["features-h4']
+topContentH[1].textContent = siteContent['main-content"]["about-h4']
+
+const topContentP = document.querySelectorAll('.top-content p')
+topContentP[0].textContent = siteContent['main-content']['features-content']
+topContentP[1].textContent = siteContent['main-content']['about-content']
+
+const midImg = document.getElementById('middle-img')
+midImg.setAttribute("src", siteContent['main-content']['middle-img-src'])
+
+const botContentH = document.querySelectorAll('.bottom-content h4')
+botContentH[0].textContent = siteContent['main-content']['services-h4']
+botContentH[1].textContent = siteContent['main-content']['product-h4']
+botContentH[2].textContent = siteContent['main-content']['vision-h4']
+
+const botContentP = document.querySelectorAll('.bottom-content p')
+botContentP[0].textContent = siteContent['main-content']['services-content']
+botContentP[1].textContent = siteContent['main-content']['product-content']
+botContentP[2].textContent = siteContent['main-content']['vision-content']
+
+const contactH4 = document.querySelector('.contact h4')
+contactH4.textContent = 'Contact'
+
+const contentP = document.querySelectorAll('.contact p')
+contentP[0].textContent = siteContent['contact']['address']
+contentP[1].textContent = siteContent['contact']['phone']
+contentP[2].textContent = siteContent['contact']['email']
+
+const footerP = document.querySelector('footer p')
+footerP.textContent = siteContent['footer']['copyright']
